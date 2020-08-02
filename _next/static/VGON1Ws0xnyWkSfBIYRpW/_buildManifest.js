a5ec533e4516e9bea8d54d9c6c35ed56cad1e6e9
jojo43 <jojo43@users.noreply.github.com>
2020-08-02 11:53:01 +0000

    deploy: 5275c33cf507a89c356e08de0ebcd66a70807ee1

---
+++ _next/static/VGON1Ws0xnyWkSfBIYRpW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/archives":[a],"/index":[a]}}("static\u002Fchunks\u002F158ade47c11c7c002ccc474e8c1ca241dedd9890.9a67ee5ae674dbefe2a8.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
